f4a088cc9648d2a065eebb64805b94fbfeb6a3f5
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-05 18:21:23 +0100

    Implement main() main

--- src/main/java/com/codecool/krk/FlattenList.java
+++ src/main/java/com/codecool/krk/FlattenList.java
@@ -1,5 +1,6 @@
 package com.codecool.krk;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,5 +16,24 @@ public class FlattenList {
             }
         }
         return flattenedList;
+    }
+
+    public static void main( String[] args ) {
+        // Example list 1
+        // [1, 3, "codecool", 5, 8]
+        List<Object> test1 = Arrays.asList(1, 3, "codecool", 5, 8);
+
+        // Example list 2
+        // [ 1, 3, ["age", 5], "Kraków", [1, [3, [4, 5], "codecool"], 6, 7]]
+        List<Object> test2 = new LinkedList<>();
+        test2.add(Arrays.asList(1, 3));
+        test2.add(Arrays.asList("age", 5));
+        test2.add("Kraków");
+        test2.add(Arrays.asList(1,Arrays.asList(3, Arrays.asList(4, 5), "codecool"), 6, 7));
+
+        FlattenList flattenList = new FlattenList();
+
+        System.out.println(flattenList.flatten(test1));
+        System.out.println(flattenList.flatten(test2));
     }
 }
