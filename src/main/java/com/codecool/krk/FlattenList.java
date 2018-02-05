169bf825ee09e96447287de1e4751e21c3a701f3
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-05 18:20:58 +0100

    Implement flatten() method

--- src/main/java/com/codecool/krk/FlattenList.java
+++ src/main/java/com/codecool/krk/FlattenList.java
@@ -1,5 +1,19 @@
 package com.codecool.krk;
 
-public class FlattenList {
+import java.util.LinkedList;
+import java.util.List;
 
+public class FlattenList {
+    public List<Object> flatten(List<Object> listToFlatten) {
+        List<Object> flattenedList = new LinkedList<>();
+
+        for (Object element: listToFlatten) {
+            if (element instanceof List) {
+                flattenedList.addAll(flatten((List<Object>) element));
+            } else {
+                flattenedList.add(element);
+            }
+        }
+        return flattenedList;
+    }
 }
